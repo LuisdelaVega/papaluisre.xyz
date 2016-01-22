237241e004e6a62720b6a45294f98e9a0b915c51
LuisdelaVega <luisr.delavega@gmail.com>
2016-01-21 22:35:44 -0500

    Playing around with Semantic UI and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ var cache = require('gulp-cache'); // Self explanatory. Caches stuff
 var del = require('del'); // Cleanes up generated files automatically. Makes sure that files that are no longer used don't remain anywhere without us knowing.
 var runSequence = require('run-sequence'); // Self explanatory. Runs tasks in a specific order
 
+// For less css
+var less = require('gulp-less');
+var path = require('path');
+
 gulp.task('hello', function() {
   console.log('Hi');
 });
@@ -23,9 +27,21 @@ gulp.task('sass', function(){
     }));
 });
 
+gulp.task('less', function () {
+  return gulp.src('app/less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({ // Allows Browser Sync to inject new CSS styles (update the CSS) into the browser whenever the sass task is ran
+      stream: true
+    }));
+});
+
 // The second argument tells Gulp that those tasks must be completed before watch is allowed to run. NOTE: Gulp runs this tasks (second argument) simultaneously
-gulp.task('watch', ['browserSync', 'sass'], function(){
+gulp.task('watch', ['browserSync', 'sass', 'less'], function(){
   gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']); // Watches for changes on these files and runs all the tasks in the array on the second parameter
+  gulp.watch('app/less/**/*.less', ['less']); // Watches for changes on these files and runs all the tasks in the array on the second parameter
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
 });
@@ -73,10 +89,10 @@ gulp.task('cache:clear', function (callback) {
 
 gulp.task('build', function (callback) {
   // Tasks in an array are run simultaneously
-  runSequence('clean:dist', ['sass', 'useref', 'images', 'fonts'], callback);
+  runSequence('clean:dist', ['sass', 'less', 'useref', 'images', 'fonts'], callback);
 });
 
 // To run default we just need to type gulp
 gulp.task('default', function (callback) {
-  runSequence(['sass','browserSync', 'watch'], callback);
+  runSequence(['sass', 'less', 'browserSync', 'watch'], callback);
 });
